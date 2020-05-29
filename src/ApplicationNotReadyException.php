462c46f7edc3b375784647aa7c6d2bc260c53fe7
Vladislav <raytracer@yandex.ru>
2020-05-29 19:00:19 +0500

    added ApplicationNotReadyException, error code is swapped with ServiceDidNotRespondException

--- src/ApplicationNotReadyException.php
+++ src/ApplicationNotReadyException.php
@@ -6,10 +6,10 @@ namespace merlinface\Exceptions;
 use Exception;
 use Throwable;
 
-class ServiceDidNotRespondException extends Exception
+class ApplicationNotReadyException extends Exception
 {
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message, 1501, $previous);
+        parent::__construct($message, 1500, $previous);
     }
 }
